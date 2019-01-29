83e852cc31e03ac8b861fd4e94408565ba6a670e
David Edelstein <yitzedelstein@gmail.com>
2019-01-29 16:44:36 -0500

    Model/Fields for registration/action result to new register view

--- CoffeeShopApp/CoffeeShopApp/Controllers/HomeController.cs
+++ CoffeeShopApp/CoffeeShopApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeShopApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,24 @@ namespace CoffeeShopApp.Controllers
 
             return View();
         }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(NewUser newUser )
+        {
+            return View("AddUser", newUser);
+        }
+
+        //public ActionResult AddUser()
+        //{
+        //    return View();
+        //}
+
+        
 
         public ActionResult About()
         {
